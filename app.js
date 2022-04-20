eec1f2af1455c448009e908798b866c95b77cf71
dalal aljassem <dalal.aljassem@gmail.com>
2022-04-20 10:10:22 +0300

    template creation done

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-// Your code goes here
+
+function SendEmail()
+{
+    var outlookApp = new ActiveXObject("Outlook.Application");
+    var nameSpace = outlookApp.getNameSpace("MAPI");
+    mailFolder = nameSpace.getDefaultFolder(6);
+    mailItem = mailFolder.Items.add('IPM.Note.FormA');
+    mailItem.Subject = "Phase 1.5 training completed";
+    mailItem.To = "dalal.aljassem@gmail.com";
+    mailItem.HTMLBody = "Hello, this is to confirm that I have complete Phase 1.5 training modules";
+    mailItem.display(0);
+}
