d8dc003c365a4d443249e1e3751f8d1d90c24802
dalal aljassem <dalal.aljassem@gmail.com>
2022-04-20 12:36:26 +0300

    emailing works

--- app.js
+++ app.js
@@ -1,12 +1,46 @@
 
-function SendEmail()
-{
-    var outlookApp = new ActiveXObject("Outlook.Application");
-    var nameSpace = outlookApp.getNameSpace("MAPI");
-    mailFolder = nameSpace.getDefaultFolder(6);
-    mailItem = mailFolder.Items.add('IPM.Note.FormA');
-    mailItem.Subject = "Phase 1.5 training completed";
-    mailItem.To = "dalal.aljassem@gmail.com";
-    mailItem.HTMLBody = "Hello, this is to confirm that I have complete Phase 1.5 training modules";
-    mailItem.display(0);
+// Your code goes here
+
+//converting excel to JSON
+const excelToJson = require("convert-excel-to-json");
+const result = excelToJson({
+  sourceFile: "names.xlsx",
+});
+
+// initializing my variables (DD/MM/YYYY)
+let grade = "A";
+let date = new Date();
+let month = date.getMonth();
+let year = date.getFullYear();
+let day = date.getDate();
+
+// Sending my mail 
+const sgMail = require('@sendgrid/mail')
+sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+const msg = {
+  to: 'dalal.aljassem@gmail.com', // Change to your recipient -> change to email variable
+  from: 'dalal.aljassem@gmail.com', // Change to your verified sender
+  subject: 'Certificate of completion',
+  text: 'and easy to do anywhere, even with Node.js',
+  html: '<strong>and easy to do anywhere, even with Node.js</strong>', // -> change to html page
 }
+sgMail
+  .send(msg)
+  .then(() => {
+    console.log('Email sent')
+  })
+  .catch((error) => {
+    console.error(error)
+  })
+
+// looping through my JSON 
+result.Sheet1.forEach(function(obj){
+    // console.log(obj.A); //name
+    // console.log(obj.B); //email
+    // console.log(obj.C); //html css js
+    // console.log(obj.D); //grade
+    let name = obj.A;
+    let email = obj.B;
+    let info = obj.C;
+    let grade = obj.D;
+})
